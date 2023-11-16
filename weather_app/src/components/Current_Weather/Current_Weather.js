8357aebeb76c5534fb7f9f80c4b0e6ecf80a52f1
Pramu-99 <pramuxjaya99@gmail.com>
2023-11-16 10:55:07 +0530

    CSS files changed
    made changes in the Current_Weather.css file

--- weather_app/src/components/Current_Weather/Current_Weather.js
+++ weather_app/src/components/Current_Weather/Current_Weather.js
@@ -9,6 +9,30 @@ const Current_Weather = () => {
                     <p className='weather_desc'>Sunny</p>
                 </div>
             </div>
+            <div className='bottom'>
+                <p className='temperature'>19 °C</p>
+                <div className='details'>
+                    <div className='parameter-row'>
+                        <span className='parameter-label'>Feels Like</span>
+                        <span className='parameter-value'>22 °C</span>
+                    </div>
+
+                    <div className='parameter-row'>
+                        <span className='parameter-label'>Wind</span>
+                        <span className='parameter-value'>2 m/s</span>
+                    </div>
+
+                    <div className='parameter-row'>
+                        <span className='parameter-label'>Humidity</span>
+                        <span className='parameter-value'>15%</span>
+                    </div>
+
+                    <div className='parameter-row'>
+                        <span className='parameter-label'>Pressure</span>
+                        <span className='parameter-value'>15hPa</span>
+                    </div>
+                </div>
+            </div>
             <img alt='weather' className='weather_icon' src='icons/01d.png' />
         </div>
     )
